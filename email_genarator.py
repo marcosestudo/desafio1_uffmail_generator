383a034c9eddae5e5fe377c5f99f38975ec46d83
Marcos Fernando <estudogit@outlook.com>
2022-08-26 14:22:41 -0300

    adicionados module docstring e comentario sobre o for da leitura do arquivo

--- email_genarator.py
+++ email_genarator.py
@@ -1,3 +1,8 @@
+""" uffmail generator.
+Reads a .csv file with students informations, searches for a student by enrollment number
+and returns a list of uffmail options based on the student's name.
+"""
+
 from csv import reader
 
 registration_number = input("Insira o número da matrícula:\n")
@@ -5,6 +10,7 @@ registration_number = input("Insira o número da matrícula:\n")
 with open("alunos.csv", encoding='utf_8') as arq:
     leitor_csv = reader(arq, delimiter=',')
 
+    # each iteration of for reads a row as an array
     for row in leitor_csv:
         if registration_number == row[1]:
             print(f"Nome: {row[0]}")
