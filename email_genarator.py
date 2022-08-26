a060bab47a6b87d31144b41bb200d58144dcbdcf
Marcos Fernando <estudogit@outlook.com>
2022-08-26 15:02:47 -0300

    funcao csv_reader convertida em um metodo estatico da classe Student

--- email_genarator.py
+++ email_genarator.py
@@ -5,30 +5,63 @@ and returns a list of uffmail options based on the student's name.
 
 from csv import reader
 
-registration_number = input("Insira o número da matrícula:\n")
+enrollment_number = input("Insira o número da matrícula:\n")
 
-class Aluno:
+class Student:
+    """ Class of student containing name, enrollment, telephone, email, uffmail (if any)
+    and subscription status (active or inactive);
+    method for reading .csv file with student information and
+    uffmail generation method based on student name
     """
-    Class docstring
-    """
-
-    def __init__(self, nome, matricula, telefone, email, uffmail, status):
-        self.__nome = nome
-        self.__matricula = matricula
-        self.__telefone = telefone
+    def __init__(self, name, enrollment, phone, email, uffmail, status):
+        self.__name = name
+        self.__enrollment = enrollment
+        self.__phone = phone
         self.__email = email
         self.__uffmail = uffmail
         self.__status = status
 
 
-def csv_reader(file):
-    with open(file, encoding='utf_8') as arq:
-        leitor_csv = reader(arq, delimiter=',')
+    @staticmethod
+    def csv_reader(file):
+        with open(file, encoding='utf_8') as arq:
+            leitor_csv = reader(arq, delimiter=',')
 
-        # each iteration of for reads a row as an array
-        for row in leitor_csv:
-            if registration_number == row[1]:
-                print(f"Nome: {row[0]}")
-                break
+            # each iteration of for reads a row as an array
+            for row in leitor_csv:
+                if enrollment_number == row[1]:
+                    print(f"Nome: {row[0]}")
+                    break
 
-csv_reader("alunos.csv")
+
+    def get_name(self):
+        """ returns student name """
+        return self.__name
+
+
+    def get_enrollment(self):
+        """ returns student enrollment number """
+        return self.__enrollment
+
+
+    def get_phone(self):
+        """ returns student phone number """
+        return self.__phone
+
+
+    def get_email(self):
+        """ returns student email """
+        return self.__email
+
+
+    def get_uffmail(self):
+        """ returns student uffmail """
+        return self.__uffmail
+
+
+    def get_status(self):
+        """ returns student subscription status """
+        return self.__status
+
+
+Student.csv_reader("alunos.csv")
