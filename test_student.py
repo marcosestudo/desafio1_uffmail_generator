973cf85c62331e6bf01e15295505bd48bc6692ca
Marcos Fernando <estudogit@outlook.com>
2022-08-31 09:18:22 -0300

    adicionados testes pra checagem de emails ja exitentes antes da geracao das opcoes

--- test_student.py
+++ test_student.py
@@ -28,26 +28,33 @@ def reset_file(file='alunos.csv'):
 
 
 class TestStudentMethods(TestCase):
+    file = 'alunos.csv'
+
+
     def test_student_name(self):
 		# Tests if the student returned by the Student.csv_reader() method is correct by name
         reset_file()
-        self.assertEqual(Student.csv_reader('alunos.csv', '105794').get_name(), 'Luiza Fernandes Ferreira')
 
-        self.assertEqual(Student.csv_reader('alunos.csv', '100503').get_name(), 'Vitor Fernandes Costa')
 
-        self.assertEqual(Student.csv_reader('alunos.csv', '105473').get_name(), 'Gabriela Pereira Araujo')
+        self.assertEqual(Student.csv_reader(self.file, '105794').get_name(), 'Luiza Fernandes Ferreira')
 
-        self.assertEqual(Student.csv_reader('alunos.csv', '123456'), 0)
+        self.assertEqual(Student.csv_reader(self.file, '100503').get_name(), 'Vitor Fernandes Costa')
+
+        self.assertEqual(Student.csv_reader(self.file, '105473').get_name(), 'Gabriela Pereira Araujo')
+
+        self.assertEqual(Student.csv_reader(self.file, '123456'), 0)
 
 
     def test_student_uffmail_options_generator(self):
 		# Tests uffmail array returned by the method uffmail_options_generator()
         reset_file()
-        self.assertEqual(Student.csv_reader('alunos.csv', '105794').uffmail_options_generator(), 0)
+        
 
-        self.assertEqual(Student.csv_reader('alunos.csv', '101369').uffmail_options_generator(), 0)
+        self.assertEqual(Student.csv_reader(self.file, '105794').uffmail_options_generator(self.file), 0)
 
-        self.assertEqual(Student.csv_reader('alunos.csv', '100591').uffmail_options_generator(), [
+        self.assertEqual(Student.csv_reader(self.file, '101369').uffmail_options_generator(self.file), 0)
+
+        self.assertEqual(Student.csv_reader(self.file, '100591').uffmail_options_generator(self.file), [
             'lucasoliveirabarros@id.uff.br',
             'lucasoliveira@id.uff.br',
             'lucasbarros@id.uff.br',
@@ -65,39 +72,140 @@ class TestStudentMethods(TestCase):
     def test_student_uffmail_creator(self, mocked_input):
         # Tests uffmail created by the method uffmail_creator()
         reset_file()
-        file = 'alunos.csv'
+        
 
         student = Student.csv_reader("alunos.csv", '109647')
-        options = student.uffmail_options_generator()
+        options = student.uffmail_options_generator(self.file)
         mocked_input.side_effect = ['7']
         chosen_option = student.options_printer(options)
-        self.assertEqual(student.uffmail_creator(chosen_option, file), 'gsribeiro@id.uff.br')
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'gsribeiro@id.uff.br')
 
         student = Student.csv_reader("alunos.csv", '111111')
-        options = student.uffmail_options_generator()
+        options = student.uffmail_options_generator(self.file)
         mocked_input.side_effect = ['12']
         chosen_option = student.options_printer(options)
-        self.assertEqual(student.uffmail_creator(chosen_option, file), 'alfabravocharlied@id.uff.br')
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'alfabravocharlied@id.uff.br')
 
         student = Student.csv_reader("alunos.csv", '222222')
-        options = student.uffmail_options_generator()
+        options = student.uffmail_options_generator(self.file)
         mocked_input.side_effect = ['1']
         chosen_option = student.options_printer(options)
-        self.assertEqual(student.uffmail_creator(chosen_option, file), 'joaoameliocorintio@id.uff.br')
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'joaoameliocorintio@id.uff.br')
 
         student = Student.csv_reader("alunos.csv", '333333')
-        options = student.uffmail_options_generator()
+        options = student.uffmail_options_generator(self.file)
         mocked_input.side_effect = ['a', '@', '*', '/', '+', '.', '!', '=', '5.5', '3,1', '5']
         chosen_option = student.options_printer(options)
-        self.assertEqual(student.uffmail_creator(chosen_option, file), 'nome_ccccc@id.uff.br')
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'nome_ccccc@id.uff.br')
 
 
     def test_password_sender(self):
 		# Tests the return of the method passord_sender()
         reset_file()
-        self.assertEqual(Student.csv_reader('alunos.csv', '105794').password_sender(), 'Success')
+        
 
-        self.assertEqual(Student.csv_reader('alunos.csv', '100406').password_sender(), 'err - inexistent uffmail')
+        self.assertEqual(Student.csv_reader(self.file, '105794').password_sender(), 'Success')
+
+        self.assertEqual(Student.csv_reader(self.file, '100406').password_sender(), 'err - inexistent uffmail')
+
+
+    @mock.patch('student.input', create=True)
+    def test_student_uffmail_options_generator_email_repetition(self, mocked_input):
+        # Tests to see the output from uffmail_options_generator to check if repeated uffmails are generated
+        reset_file()
+
+        student = Student.csv_reader(self.file, '444444')
+        options = student.uffmail_options_generator(self.file)
+        mocked_input.side_effect = ['1']
+        chosen_option = student.options_printer(options)
+        self.assertEqual(options, [
+            'nomemuitocomum@id.uff.br',
+            'nomemuito@id.uff.br',
+            'nomecomum@id.uff.br',
+            'nome_muito@id.uff.br',
+            'nome_comum@id.uff.br',
+            'nmuitocomum@id.uff.br',
+            'nmcomum@id.uff.br',
+            'nomemcomum@id.uff.br',
+            'nomemc@id.uff.br',
+            'nomemuitoc@id.uff.br'
+        ])
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'nomemuitocomum@id.uff.br')
+
+        student = Student.csv_reader(self.file, '555555')
+        options = student.uffmail_options_generator(self.file)
+        mocked_input.side_effect = ['1']
+        chosen_option = student.options_printer(options)
+        self.assertEqual(options, [
+            'nomemuito@id.uff.br',
+            'nomecomum@id.uff.br',
+            'nome_muito@id.uff.br',
+            'nome_comum@id.uff.br',
+            'nmuitocomum@id.uff.br',
+            'nmcomum@id.uff.br',
+            'nomemcomum@id.uff.br',
+            'nomemc@id.uff.br',
+            'nomemuitoc@id.uff.br'
+        ])
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'nomemuito@id.uff.br')
+
+        student = Student.csv_reader(self.file, '666666')
+        options = student.uffmail_options_generator(self.file)
+        mocked_input.side_effect = ['1']
+        chosen_option = student.options_printer(options)
+        self.assertEqual(options, [
+            'nomecomum@id.uff.br',
+            'nome_muito@id.uff.br',
+            'nome_comum@id.uff.br',
+            'nmuitocomum@id.uff.br',
+            'nmcomum@id.uff.br',
+            'nomemcomum@id.uff.br',
+            'nomemc@id.uff.br',
+            'nomemuitoc@id.uff.br'
+        ])
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'nomecomum@id.uff.br')
+
+        student = Student.csv_reader(self.file, '777777')
+        options = student.uffmail_options_generator(self.file)
+        mocked_input.side_effect = ['7']
+        chosen_option = student.options_printer(options)
+        self.assertEqual(options, [
+            'nome_muito@id.uff.br',
+            'nome_comum@id.uff.br',
+            'nmuitocomum@id.uff.br',
+            'nmcomum@id.uff.br',
+            'nomemcomum@id.uff.br',
+            'nomemc@id.uff.br',
+            'nomemuitoc@id.uff.br'
+        ])
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'nomemuitoc@id.uff.br')
+
+        student = Student.csv_reader(self.file, '888888')
+        options = student.uffmail_options_generator(self.file)
+        mocked_input.side_effect = ['6']
+        chosen_option = student.options_printer(options)
+        self.assertEqual(options, [
+            'nome_muito@id.uff.br',
+            'nome_comum@id.uff.br',
+            'nmuitocomum@id.uff.br',
+            'nmcomum@id.uff.br',
+            'nomemcomum@id.uff.br',
+            'nomemc@id.uff.br'
+        ])
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'nomemc@id.uff.br')
+
+        student = Student.csv_reader(self.file, '999999')
+        options = student.uffmail_options_generator(self.file)
+        mocked_input.side_effect = ['5']
+        chosen_option = student.options_printer(options)
+        self.assertEqual(options, [
+            'nome_muito@id.uff.br',
+            'nome_comum@id.uff.br',
+            'nmuitocomum@id.uff.br',
+            'nmcomum@id.uff.br',
+            'nomemcomum@id.uff.br',
+        ])
+        self.assertEqual(student.uffmail_creator(chosen_option, self.file), 'nomemcomum@id.uff.br')
 
 
 if __name__ == '__main__':
